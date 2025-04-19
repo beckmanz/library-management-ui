b3e62c8acd983b1f8db413c513a5748cd47eba6b
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-04-19 01:51:07 -0300

    feat: adiciona logica para redirecionar quando autenticado

--- src/routes/public-route.tsx
+++ src/routes/public-route.tsx
@@ -1,5 +1,7 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
 
 export default function PublicRoute() {
-  return <Outlet />;
+  const { user } = useAuth();
+  return !user ? <Outlet /> : <Navigate to="/dashboard" replace />;
 }
