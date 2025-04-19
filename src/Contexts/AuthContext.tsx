2952b4309c1f6810ea712f4016d562c5948d4d7e
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-04-19 01:55:06 -0300

    feat: cria AuthContext para o gerenciamente do estado global de autenticação

---
+++ src/Contexts/AuthContext.tsx
@@ -0,0 +1,16 @@
+// @refresh reset
+import { createContext } from "react";
+
+export type User = {
+  id: string;
+  name: string;
+  email: string;
+};
+
+type AuthContextType = {
+  user: User | null;
+  signin: (email: string, password: string) => Promise<void>;
+  signup: (name: string, email: string, password: string) => Promise<void>;
+};
+
+export const AuthContext = createContext({} as AuthContextType);
