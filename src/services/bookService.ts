d6204649441def5d150da42640d1f2d980e8b8c6
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-04-25 23:41:52 -0300

    feat: cria bookService com metodos addNewBook, fetchBooks, fetchBooksByTitle e deleteBook

---
+++ src/services/bookService.ts
@@ -0,0 +1,74 @@
+import { newBookSchema } from "@/components/forms/createNewBook";
+import api from "../config/api";
+import { Author, getAuthorById } from "./authorService";
+import { Book } from "lucide-react";
+
+export interface Book {
+  id: string;
+  title: string;
+  genre: string;
+  publicationYear: number;
+  isAvailable: boolean;
+  addedAt: string;
+  authorId: string;
+  author: Author;
+  libraryId: string;
+}
+
+export async function addNewBook(data: newBookSchema) {
+  try {
+    const response = await api.post("/book", {
+      title: data.title,
+      genre: data.genre,
+      publicationYear: data.publicationYear,
+      authorId: data.authorId,
+    });
+    const author = await getAuthorById(data.authorId);
+    const newBook: Book = { ...response.data.data, author };
+    return newBook;
+  } catch (error) {
+    console.log("Erro ao tentar adicionar o livro a biblioteca", error, data);
+    throw error;
+  }
+}
+
+export async function fetchBooks() {
+  try {
+    const response = await api.get("/book");
+    const books = await Promise.all(
+      response.data.data.map(async (book: Book) => {
+        const author = await getAuthorById(book.authorId);
+        return { ...book, author };
+      }),
+    );
+    return books;
+  } catch (error) {
+    console.error("Error fetching books:", error);
+    throw error;
+  }
+}
+
+export async function fetchBooksByTitle(title: string) {
+  try {
+    const response = await api.get(`/book/bytitle/${title}`);
+    const books: Book[] = await Promise.all(
+      response.data.data.map(async (book: any) => {
+        const author = await getAuthorById(book.authorId);
+        return { ...book, author };
+      }),
+    );
+    return books;
+  } catch (error) {
+    console.error("Erro ao buscar o livro:", error, title);
+    throw error;
+  }
+}
+
+export async function deleteBook(bookId: string) {
+  try {
+    await api.delete(`/book/${bookId}`);
+  } catch (error) {
+    console.error("Erro ao tentar deletar livro", error, bookId);
+    throw error;
+  }
+}
