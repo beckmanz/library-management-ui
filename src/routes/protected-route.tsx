668afee4bc33bfee56fefb1076b493df3910f51f
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-04-26 16:03:05 -0300

    feat: adiciona toaster ao layout

--- src/routes/protected-route.tsx
+++ src/routes/protected-route.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 import Sidebar from "../components/sidebar";
+import { Toaster } from "sonner";
 
 export default function ProtectedRoute() {
   const { user } = useAuth();
@@ -10,6 +11,7 @@ export default function ProtectedRoute() {
       <div className="flex-1 overflow-auto">
         <Outlet />
       </div>
+      <Toaster />
     </div>
   ) : (
     <Navigate to="/signin" replace />
--- src/routes/public-route.tsx
+++ src/routes/public-route.tsx
@@ -1,7 +1,15 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
+import { Toaster } from "sonner";
 
 export default function PublicRoute() {
   const { user } = useAuth();
-  return !user ? <Outlet /> : <Navigate to="/dashboard" replace />;
+  return !user ? (
+    <div className="flex min-h-screen min-w-screen">
+      <Outlet />
+      <Toaster />
+    </div>
+  ) : (
+    <Navigate to="/dashboard" replace />
+  );
 }
