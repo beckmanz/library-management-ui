75c9d5beade19b3ea7af75b3cda61ba73e3a6ed4
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-05-02 15:08:12 -0300

    feat: cria arquivos das paginas de autor, readers e loans

---
+++ src/screens/dashboard/loans.tsx
@@ -0,0 +1,7 @@
+export default function Loans() {
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <h1 className="text-2xl font-bold">Dashboard loans</h1>
+    </div>
+  );
+}
---
+++ src/screens/dashboard/readers.tsx
@@ -0,0 +1,7 @@
+export default function Readers() {
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <h1 className="text-2xl font-bold">Dashboard readers</h1>
+    </div>
+  );
+}
